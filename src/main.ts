ad94424ce5c3b93dc9b35d93bff59e0f55ae9cef
TakumiYoshida <yoshida.fm.0626@gmail.com>
2025-11-24 10:21:06 +0900

    feat: add API endpoint to retrieve latest game turns and board state

--- src/main.ts
+++ src/main.ts
@@ -38,13 +38,8 @@ app.get('/api/error', async (req, res) => {
 // ゲーム開始
 app.post('/api/games', async (req, res) => {
   const now = new Date();
-  const conn = await mysql.createConnection({
-    host: 'localhost',
-    port: 3307,
-    database: 'reversi',
-    user: 'reversi',
-    password: 'password',
-  });
+  const conn = await connectMySql();
+
   try {
     await conn.beginTransaction();
 
@@ -91,6 +86,53 @@ app.post('/api/games', async (req, res) => {
   res.status(201).end();
 });
 
+app.get('/api/games/latest/turns/:turnCount', async (req, res) => {
+  const turnCount = parseInt(req.params.turnCount);
+
+  const conn = await connectMySql();
+  try {
+    // 最新のゲームを取得する
+    const gameSelectResult = await conn.execute<mysql.RowDataPacket[]>(
+      'select id, started_at from games order by id desc limit 1'
+    );
+    const game = gameSelectResult[0][0];
+
+    // 現在のターン情報を取得する
+    const turnSelectResult = await conn.execute<mysql.RowDataPacket[]>(
+      'select id, game_id, turn_count, next_disc, end_at from turns where game_id= ? and turn_count = ?',
+      [game['id'], turnCount]
+    );
+    const turn = turnSelectResult[0][0];
+
+    // 選択したマスの状態を取得
+    const squaresSelectResult = await conn.execute<mysql.RowDataPacket[]>(
+      'select id turn_id, x, y, disc from squares where turn_id = ?',
+      [turn['id']]
+    );
+    const squares = squaresSelectResult[0];
+
+    // ８*8の多重配列を作成（ボード）
+    const board = Array.from(Array(8).map(() => Array.from(Array(8))));
+
+    // 盤面全体の状態を配列に格納
+    squares.forEach((s) => {
+      board[s.y][s.x] = s.disc;
+    });
+
+    const responseBody = {
+      turnCount,
+      board,
+      nextDisc: turn['next_disc'],
+      // TODO 決着がついている場合、game_resultsテーブルから取得する
+      winnerDisc: null,
+    };
+
+    res.json(responseBody);
+  } finally {
+    await conn.end();
+  }
+});
+
 app.use(errorHandler);
 
 app.listen(PORT, () => {
@@ -106,5 +148,15 @@ function errorHandler(
   console.error('Unexpected error occurred', err);
   res.status(500).send({
     message: 'Unexpected error occurred',
+  });
+}
+
+async function connectMySql() {
+  return await mysql.createConnection({
+    host: 'localhost',
+    port: 3307,
+    database: 'reversi',
+    user: 'reversi',
+    password: 'password',
   });
 }
